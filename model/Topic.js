19038109eb63736e67cc228c7b7739e63aaca920
Chones17 <chones17@byui.edu>
2023-03-06 14:36:21 -0700

    Create Topic.js
    Refactor model to include title and description

---
+++ model/Topic.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const topicSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Topic', topicSchema);
