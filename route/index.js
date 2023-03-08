2efd440e7ccd3a0036bbb5a28877701d50c4d414
Zachary Thompson <tho01699@ttu.edu>
2023-03-08 08:59:04 -0600

    swagger autogen

--- route/index.js
+++ route/index.js
@@ -1,6 +1,7 @@
 const route = require('express').Router();
 
 route.get('/', require('../controller/index'));
+route.use('/', require('./swagger'));
 route.get('/journal', require('../route/journal'));
 route.get('/entry', require('../route/entry'));
 route.get('/topic', require('../route/topic'));
---
+++ route/swagger.js
@@ -0,0 +1,11 @@
+const route = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swaggerDesign.json');
+
+
+
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+module.exports = route;
