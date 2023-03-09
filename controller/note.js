57c01af7ad86d0fafc2fd3b49d8db98a3b2070ee
Zachary Thompson <tho01699@ttu.edu>
2023-03-09 12:03:02 -0600

    Figured out the post with mongoose

--- controller/note.js
+++ controller/note.js
@@ -1,57 +1,42 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
+
+
+var Note = require('../model/Note')
 
 
 //Get all
 const getNotes = async (req, res, next) => {
-    try {
-    mongodb.getDb().db().collection('notes').find().toArray((err, lists) => {
-      if (err){
-        res.status(400).json({message: err});
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-    } catch (err){
-      res.status(500).json(err);
-    }
+    const result = Note.find({});
+    console.log(result);
+  
+
     };
 
 const getNote = async (req, res, next) => {
-    try {
+  
         //  #swagger.parameters['id'] = { description: 'Get a specfic worker' }
     const userId = new ObjectId(req.params.id);
-    mongodb.getDb().db().collection('notes').find({ _id: userId }).toArray((err, result) => {
-        if (err){
-        res.status(400).json({message: err});
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result[0]);
-    });
-    } catch (err){
-    res.status(500).json(err);
-    }
+    const result = Note.find({ _id: userId });
+   
+    console.log(result);
+    
     };
+  
 
 const createNote = async (req, res) => {
-  const entry = {
+    var note = new Note({
     entryDate: req.body.entryDate,
     canon: req.body.canon,
     book: req.body.book,
     chapter: req.body.chapter,
     verse: req.body.verse,
     note: req.body.note
-  };
-  const result = await mongodb
-  .getDb()
-  .db()
-  .collection('notes')
-  .insertOne(entry);
-  if (result.acknowledged) {
-    res.status(201).json(result);
-  } else {
-    res.status(500).json(result.error || 'Some error occurred while creating the note.');
-  }
+  })
+  const result = await note.save()
+  console.log(result);
+
 }
 
 module.exports = {getNotes, getNote, createNote};
