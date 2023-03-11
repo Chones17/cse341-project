a5b0a5ed0fbe2d35a63c37ce8e4414f90e3b7c8c
Chones17 <chones17@byui.edu>
2023-03-10 20:10:56 -0700

    Update swagger.js
    Refactor API documents to include note/:topic and note/:date routes

--- route/note.js
+++ route/note.js
@@ -5,6 +5,12 @@ const controller = require('../controller/note');
 // Handles any http GET requests
 route.get('/', controller.getNotes);
 route.get('/:id', controller.getNote);
+route.get('/:date', (req, res) => {
+    // #swagger.tags = ['Note']
+    res.status(200).send('This is a date');});
+route.get('/:topic', (req, res) => {
+    // #swagger.tags = ['Note']
+    res.status(200).send('This is a topic')});
 
 // Handles any http POST requests
 route.post('/', controller.createNote);
