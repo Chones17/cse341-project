7926098ca9e8339cf09711d255c9ebeda78ffd8f
Chones17 <chones17@byui.edu>
2023-03-19 22:59:44 -0600

    Update user.js
    Add authentication to User routes

--- route/user.js
+++ route/user.js
@@ -1,19 +1,20 @@
 // Require the User router, validation middleware, and controller
 const route = require('express').Router();
 const controller = require('../controller/user');
+const auth = require('../middleware/auth');
 
 // Handles any http GET requests
-route.get('/', controller.getUsers);
-route.get('/:id', controller.getUser);
+route.get('/', auth, controller.getUsers);
+route.get('/:id', auth, controller.getUser);
 
 // Handles any http POST requests
-route.post('/', controller.createUser);
+route.post('/', auth, controller.postUser);
 
 // Handles any http PUT requests
-route.put('/:id', controller.updateUser);
+route.put('/:id', auth, controller.putUser);
 
 // Handles any http DELETE requests
-route.delete('/:id', controller.deleteUser);
+route.delete('/:id', auth, controller.deleteUser);
 
 // Export route object
 module.exports = route;
