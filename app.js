92098e83667b24a7e82e7b5bb9ec432d2a87cc1f
Chones17 <chones17@byui.edu>
2023-03-19 22:47:52 -0600

    Update app.js
    Refactor to include dependencies for OAuth and sessions

--- app.js
+++ app.js
@@ -3,17 +3,34 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connect = require('./config/connect');
+const session = require('express-session');
+const auth = require('./config/auth');
 const app = express();
+const passport = require('passport');
 const mongoose = require('mongoose');
+const MongoStore = require('connect-mongodb-session')(session);
 const port = process.env.PORT || 3000;
 
 // Connect to database
 connect();
 
+// Use Passport configuration
+auth();
+
 // Use CORS, JSON, UrlEncoded, and route for application
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ 
+        uri: process.env.MONGODB_URI, 
+        databaseName: 'library', 
+        collection: 'sessions'})}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', require('./route'));
 
 // Listens to port and logs event to the console
