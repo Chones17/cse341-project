e53e485f43d207f3db7b174bd5a92e193f5cf8af
Chones17 <chones17@byui.edu>
2023-03-19 22:50:09 -0600

    Update index.js
    Refactor Index controller functions

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,38 @@
-const home = async (req, res) => {
+/**
+ * Controller function for application
+ * @param { object } req - The HTTP request
+ * @param { object } res - The HTTP response
+ */
+const index = async (req, res) => {
+
+    // #swagger.ignore = true
+
     await res.status(200).send('This is the home page.');
 }
 
-module.exports = home;
+/**
+ * Controller function for login
+ * @param { object } req - The HTTP request
+ * @param { object } res - The HTTP response
+ */
+const login = async (req, res) => {
+
+    // #swagger.tags = ['Authentication']
+
+    await res.status(200).send('This is the login page.');
+}
+
+/**
+ * Controller function for logout
+ * @param { object } req - The HTTP request
+ * @param { object } res - The HTTP response
+ */
+const logout = async (req, res) => {
+
+    // #swagger.tags = ['Authentication']
+
+    await req.logout(() => res.status(200).redirect('/login'));
+}
+
+// Export Index objects
+module.exports = { index, login, logout };
