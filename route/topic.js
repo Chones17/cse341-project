36cacc6a0314ce4e15af0a56fb8ed4d5b28d634b
Chones17 <chones17@byui.edu>
2023-03-19 22:59:32 -0600

    Update topic.js
    Add authentication to Topic routes

--- route/topic.js
+++ route/topic.js
@@ -1,18 +1,19 @@
 // Require the Topic router, validation middleware, and controller
 const route = require('express').Router();
 const controller = require('../controller/topic');
+const auth = require('../middleware/auth');
 
 // Handles any http GET requests
-route.get('/', controller.getTopics);
+route.get('/', auth, controller.getTopics);
 
 // Handles any http POST requests
-route.post('/', controller.createTopic);
+route.post('/', auth, controller.postTopic);
 
 // Handles any http PUT requests
-route.put('/:id', controller.updateTopic);
+route.put('/:id', auth, controller.putTopic);
 
 // Handles any http DELETE requests
-route.delete('/:id', controller.deleteTopic);
+route.delete('/:id', auth, controller.deleteTopic);
 
 // Export route object
 module.exports = route;
