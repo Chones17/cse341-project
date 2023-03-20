6126004d35bca6560e04c18dfdc12759a70200a8
Chones17 <chones17@byui.edu>
2023-03-19 22:59:21 -0600

    Update note.js
    Add authentication to Note routes

--- route/note.js
+++ route/note.js
@@ -1,25 +1,20 @@
 // Require the Note router, validation middleware, and controller
 const route = require('express').Router();
 const controller = require('../controller/note');
+const auth = require('../middleware/auth');
 
 // Handles any http GET requests
-route.get('/', controller.getNotes);
-route.get('/:id', controller.getNote);
-route.get('/:date', (req, res) => {
-    // #swagger.tags = ['Note']
-    res.status(200).send('This is a date');});
-route.get('/:topic', (req, res) => {
-    // #swagger.tags = ['Note']
-    res.status(200).send('This is a topic')});
+route.get('/', auth, controller.getNotes);
+route.get('/:id', auth, controller.getNote);
 
 // Handles any http POST requests
-route.post('/', controller.createNote);
+route.post('/', auth, controller.postNote);
 
 // Handles any http PUT requests
-route.put('/:id', controller.updateNote);
+route.put('/:id', auth, controller.putNote);
 
 // Handles any http DELETE requests
-route.delete('/:id', controller.deleteNote);
+route.delete('/:id', auth, controller.deleteNote);
 
 // Export route object
 module.exports = route;
