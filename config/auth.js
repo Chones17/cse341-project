b694895b71196f4e748181d89ab06ec12b8c9bee
Chones17 <chones17@byui.edu>
2023-03-19 22:48:34 -0600

    Create auth.js
    Add GitHub OAuth configuration file to project

---
+++ config/auth.js
@@ -0,0 +1,30 @@
+// Require Passport, User model, and Auth strategy
+const passport = require('passport');
+const User = require('../model/User');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+/**
+ * Authentication configuration
+ */
+const auth = () => {
+
+    // Serialize and deserialize user session
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (error, user) => done(error, user));
+    });
+
+
+    // Passport configuration
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://cse341-project-production.onrender.com/auth/github/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        User.findOrCreate({ githubId: profile.id }, function (err, user) {
+            return done(err, user);
+    })}));
+}
+
+module.exports = auth;
