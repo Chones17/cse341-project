206b0c04efaea8b45e0fb3e3207e758154e5e570
Chones17 <chones17@byui.edu>
2023-03-30 20:00:52 -0600

    Update auth.js
    Remove GitHub authorization routes from API documentation

--- route/auth.js
+++ route/auth.js
@@ -2,11 +2,17 @@
 const route = require('express').Router();
 const passport = require('passport');
 
+// Add Authentication tags to route
+const auth = (req, res) => {
+
+        // #swagger.ignore = true
+}
+
 // Handles and GET HTTP requests
-route.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+route.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }), auth);
 route.get('/github/callback', passport.authenticate('github', { 
         successRedirect: '/',
-        failureRedirect: '/login' }));
+        failureRedirect: '/login' }), auth);
 
 // Export route object
 module.exports = route;
