0f9eff0878bcfbf9b5a0d9bbf8ea28b2923cc00e
Chones17 <chones17@byui.edu>
2023-04-05 17:28:49 -0600

    Update note.js
    Refactor route validation

--- route/note.js
+++ route/note.js
@@ -2,22 +2,37 @@
 const route = require('express').Router();
 const controller = require('../controller/note');
 const auth = require('../middleware/auth');
-
-//validation
-const validation = require('../middleware/validate');
+const validate = require('../middleware/validate');
 
 // Handles any http GET requests
 route.get('/', auth, controller.getNotes);
-route.get('/:id', auth, controller.getNote);
+route.get('/:id', auth, validate.id, validate.results, controller.getNote);
 
 // Handles any http POST requests
-route.post('/', auth, validation.saveNote, controller.postNote);
+route.post('/', auth,
+    validate.entryDate,
+    validate.canon,
+    validate.book,
+    validate.chapter,
+    validate.verse,
+    validate.note,
+    validate.results,
+    controller.postNote);
 
 // Handles any http PUT requests
-route.put('/:id', auth, validation.saveNote, controller.putNote);
+route.put('/:id', auth,
+    validate.id,
+    validate.entryDate,
+    validate.canon,
+    validate.book,
+    validate.chapter,
+    validate.verse,
+    validate.note,
+    validate.results,
+    controller.putNote);
 
 // Handles any http DELETE requests
-route.delete('/:id', auth, controller.deleteNote);
+route.delete('/:id', auth, validate.id, validate.results, controller.deleteNote);
 
 // Export route object
 module.exports = route;
