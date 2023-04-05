3ce583e648e5e57c8a83ecbce658b3f3586b1f32
Chones17 <chones17@byui.edu>
2023-04-05 17:29:00 -0600

    Update topic.js
    Refactor route validation

--- route/topic.js
+++ route/topic.js
@@ -2,21 +2,19 @@
 const route = require('express').Router();
 const controller = require('../controller/topic');
 const auth = require('../middleware/auth');
-
-//validation
-const validation = require('../middleware/validate');
+const validate = require('../middleware/validate');
 
 // Handles any http GET requests
 route.get('/', auth, controller.getTopics);
 
 // Handles any http POST requests
-route.post('/', auth, validation.saveTopic, controller.postTopic);
+route.post('/', auth, validate.topic, validate.results, controller.postTopic);
 
 // Handles any http PUT requests
-route.put('/:id', auth, validation.saveTopic, controller.putTopic);
+route.put('/:id', auth, validate.id, validate.topic, validate.results, controller.putTopic);
 
 // Handles any http DELETE requests
-route.delete('/:id', auth, controller.deleteTopic);
+route.delete('/:id', auth, validate.topic, validate.results, controller.deleteTopic);
 
 // Export route object
 module.exports = route;
