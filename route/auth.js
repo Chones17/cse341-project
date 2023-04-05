db1762cb80b073349f72c2e9ab4b0b3864e72337
Chones17 <chones17@byui.edu>
2023-04-05 17:28:27 -0600

    Update auth.js
    Refactor authentication routes for Swagger

--- route/auth.js
+++ route/auth.js
@@ -2,17 +2,12 @@
 const route = require('express').Router();
 const passport = require('passport');
 
-// Add Authentication tags to route
-const auth = (req, res) => {
-
-        // #swagger.ignore = true
-}
-
 // Handles and GET HTTP requests
-route.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }), auth);
+route.get('/github', passport.authenticate('github', { scope: [ 'user:email' ] }), 
+        (req, res) => { /* #swagger.ignore = true */ });
 route.get('/github/callback', passport.authenticate('github', { 
-        successRedirect: '/',
-        failureRedirect: '/login' }), auth);
+        successRedirect: '/', 
+        failureRedirect: '/login' }), (req, res) => { /* #swagger.ignore = true */ });
 
 // Export route object
 module.exports = route;
