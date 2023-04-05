4f746f6272d524f7b1f6bba2553439e96595e4b2
Chones17 <chones17@byui.edu>
2023-04-05 17:21:52 -0600

    Create auth.spec.js
    Add Authorization route/controller test

---
+++ __test__/auth.spec.js
@@ -0,0 +1,27 @@
+// Require SuperTest and set constants
+const request = require('supertest');
+const app = require('../app');
+
+// Test 'GET /auth/github' route
+describe ('GET /auth/github', () => {
+
+    // Test '/auth/github' status response
+    test('should return 302', async () => {
+        const response = await request(app)
+            .get('/auth/github')
+        expect(response.statusCode).toBe(302);
+    });
+
+    // Test '/auth/github/callback' status response
+    test('GET should return 302', async () => {
+        const response = await request(app).get('/auth/github/callback');
+        expect(response.statusCode).toBe(302);
+    });
+});
+
+describe('Auth function', () => {
+
+    test('should return nothing', () => {
+        expect()
+    });
+});
