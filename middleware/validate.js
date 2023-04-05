a5caae622e88b99ecdc9e21784e8703deeee88d3
Chones17 <chones17@byui.edu>
2023-04-05 17:25:42 -0600

    Update validate.js
    Refactor validation for express-validator dependency

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,91 +1,50 @@
-const validator = require('../helpers/validate');
+// Require Express-Validator
+const { param, body, validationResult } = require('express-validator');
 
-const saveJournal = (req, res, next) => {
-    const validationRule = {
-        title: 'require|string',
-        description: 'require|string',
-        createDate: 'require|Date'
-    };
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-          res.status(412).send({
-            success: false,
-            message: 'Validation failed',
-            data: err
-          });
-        } else {
-          next();
-        }
-      });
+// Define Validation Chain API
+const id = param('id', 'Invalid MongoID').isMongoId();
+const title = body('title', 'Invalid Title').isString();
+const description = body('description', 'Invalid Description').isString();
+const createDate = body('createDate', 'Invalid Date').isDate({format: "MM/DD/YYYY"});
+const entryDate = body('createDate', 'Invalid Date').isDate({format: "MM/DD/YYYY"});
+const canon = body('canon', 'Invalid Canon').isString();     
+const book = body('book', 'Invalid Book').isString();    
+const chapter = body('chapter', 'Invalid Chapter').isInt();       
+const verse = body('verse', 'Invalid Verse').isInt();     
+const note = body('note', 'Invalid note').isString();
+const topic = body('topic', 'Invalid Topic').isString();
+const githubId = body('githubId', 'Invalid GitHub ID').isString();
+const userName = body('userName', 'Invalid Userame').isString();
+const email = body('email', 'Invalid Email').isEmail();
+
+// GET route validation
+const results = (req, res, next) => {
+
+    // Return GET validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400);
+        res.json({ errors: errors.array() });
+    } else {
+        next();
+    }
 }
 
-const saveNote = (req, res, next) => {
-    const validationRule = {
-        entryDate: 'require|Date',
-        canon: ['require', 'string',{ 'in': [
-            'Old Testament', 
-            'New Testament', 
-            'Book of Mormon', 
-            'Doctrine and Covenants', 
-            'Pearl of Great Price'
-        ]}],
-        book: 'require|string',
-        chapter: 'require|integer|min:1|max:150',
-        verse: 'require|integer|min:1|max:176',
-        note: 'require|string'
-    };
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-          res.status(412).send({
-            success: false,
-            message: 'Validation failed',
-            data: err
-          });
-        } else {
-          next();
-        }
-      });
-}
-
-const saveTopic = (req, res, next) => {
-    const validationRule = {
-        topic: 'require|string'
-    };
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-          res.status(412).send({
-            success: false,
-            message: 'Validation failed',
-            data: err
-          });
-        } else {
-          next();
-        }
-      });
-}
-
-const saveUser = (req, res, next) => {
-    const validationRule = {
-        githubId: 'require|string',
-        userName: 'string',
-        email: 'string'
-    };
-    validator(req.body, validationRule, {}, (err, status) => {
-        if (!status) {
-          res.status(412).send({
-            success: false,
-            message: 'Validation failed',
-            data: err
-          });
-        } else {
-          next();
-        }
-      });
-}
-
-module.exports = {
-    saveJournal,
-    saveNote,
-    saveTopic,
-    saveUser
+// Export validation function
+module.exports = { 
+    id,
+    title,
+    description,
+    createDate,
+    entryDate,
+    canon,
+    book,
+    chapter,
+    verse,
+    note,
+    topic,
+    githubId,
+    userName,
+    email,
+    results
 };
