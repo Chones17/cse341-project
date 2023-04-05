b4a2a18a1d41b95be9d6bab2988747e5f36ee26c
Chones17 <chones17@byui.edu>
2023-04-05 17:28:39 -0600

    Update journal.js
    Refactor route validation

--- route/journal.js
+++ route/journal.js
@@ -2,22 +2,31 @@
 const route = require('express').Router();
 const controller = require('../controller/journal');
 const auth = require('../middleware/auth');
-
-//validation
-const validation = require('../middleware/validate');
+const validate = require('../middleware/validate');
 
 // Handles any http GET requests
 route.get('/', auth, controller.getJournals);
-route.get('/:id', auth, controller.getJournal);
+route.get('/:id', auth, validate.id, validate.results, controller.getJournal);
 
 // Handles any http POST requests
-route.post('/', auth, validation.saveJournal, controller.postJournal);
+route.post('/', auth, 
+    validate.title, 
+    validate.description, 
+    validate.createDate,
+    validate.results,
+    controller.postJournal);
 
 // Handles any http PUT requests
-route.put('/:id', auth, validation.saveJournal, controller.putJournal);
+route.put('/:id', auth,
+    validate.id,
+    validate.title,
+    validate.description,
+    validate.createDate,
+    validate.results,
+    controller.putJournal);
 
 // Handles any http DELETE requests
-route.delete('/:id', auth, controller.deleteJournal);
+route.delete('/:id', auth, validate.id, validate.results, controller.deleteJournal);
 
 // Export route object
 module.exports = route;
