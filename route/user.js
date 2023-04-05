aac2c781628e92a40eb77f50022fa625fb92c14c
Chones17 <chones17@byui.edu>
2023-04-05 17:29:10 -0600

    Update user.js
    Refactor route validation

--- route/user.js
+++ route/user.js
@@ -2,22 +2,31 @@
 const route = require('express').Router();
 const controller = require('../controller/user');
 const auth = require('../middleware/auth');
-
-//validation
-const validation = require('../middleware/validate');
+const validate = require('../middleware/validate');
 
 // Handles any http GET requests
 route.get('/', auth, controller.getUsers);
-route.get('/:id', auth, controller.getUser);
+route.get('/:id', auth, validate.id, validate.results, controller.getUser);
 
 // Handles any http POST requests
-route.post('/', auth, validation.saveUser, controller.postUser);
+route.post('/', auth, 
+    validate.githubId, 
+    validate.userName, 
+    validate.email,
+    validate.results,
+    controller.postUser);
 
 // Handles any http PUT requests
-route.put('/:id', auth, validation.saveUser, controller.putUser);
+route.put('/:id', auth, 
+    validate.id,
+    validate.githubId, 
+    validate.userName, 
+    validate.email,
+    validate.results,
+    controller.putUser);
 
 // Handles any http DELETE requests
-route.delete('/:id', auth, controller.deleteUser);
+route.delete('/:id', auth, validate.id, validate.results, controller.deleteUser);
 
 // Export route object
 module.exports = route;
